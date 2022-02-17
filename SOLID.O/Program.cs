ebccdddf396975a600c810d5e7d48110ca195fcb
Gaspar <obertigaspar@gmail.com>
2022-02-17 20:28:17 -0300

    se agregó el proyecto para el principio open closed. Esta es la versón en la que no se considera el principio.

---
+++ SOLID.O/Program.cs
@@ -0,0 +1,8 @@
+﻿using SOLID.O;
+
+DocumentoContable factura = new DocumentoContable(TipoDocumentoContable.Factura, 12365);
+DocumentoContable notacredito = new DocumentoContable(TipoDocumentoContable.NotaCredito, 12365);
+Console.WriteLine(factura.Descripcion());
+Console.WriteLine(notacredito.Descripcion());
+Console.ReadKey();
+
