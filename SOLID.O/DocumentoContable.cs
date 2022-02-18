17425fd4852615d21e39789059452dea73151eef
Gaspar <obertigaspar@gmail.com>
2022-02-17 21:21:16 -0300

    se refactorizaron las clases para incorporar el principio

--- SOLID.O/DocumentoContable.cs
+++ SOLID.O/DocumentoContable.cs
@@ -6,29 +6,32 @@ using System.Threading.Tasks;
 
 namespace SOLID.O
 {
-    public class DocumentoContable
+    public abstract class DocumentoContable
     {
-        public DocumentoContable(TipoDocumentoContable tipo, int numero)
+        //public DocumentoContable(TipoDocumentoContable tipo, int numero)
+        public DocumentoContable(int numero)
         {
-            Tipo = tipo;
+            //Tipo = tipo;
             Numero = numero;
         }
 
         public DateTime Fecha { get; set; }
         public double Importe { get; set; }
         public int Numero { get; set; }
-        public TipoDocumentoContable Tipo { get; set; }
+        //public TipoDocumentoContable Tipo { get; set; }
 
-        public string Descripcion()
-        {
-            switch (Tipo)
-            {
-                case TipoDocumentoContable.Factura:
-                    return $"FC-{Numero}";
-                case TipoDocumentoContable.NotaCredito:
-                    return $"NC-{Numero}";
-                default: return "N/D";
-            }
-        }
+        //public string Descripcion()
+        //{
+        //    switch (Tipo)
+        //    {
+        //        case TipoDocumentoContable.Factura:
+        //            return $"FC-{Numero}";
+        //        case TipoDocumentoContable.NotaCredito:
+        //            return $"NC-{Numero}";
+        //        default: return "N/D";
+        //    }
+        //}
+
+        public abstract string Descripcion();
     }
 }
--- SOLID.O/Program.cs
+++ SOLID.O/Program.cs
@@ -1,8 +1,13 @@
 ﻿using SOLID.O;
 
-DocumentoContable factura = new DocumentoContable(TipoDocumentoContable.Factura, 12365);
-DocumentoContable notacredito = new DocumentoContable(TipoDocumentoContable.NotaCredito, 12365);
+//DocumentoContable factura = new DocumentoContable(TipoDocumentoContable.Factura, 12365);
+//DocumentoContable notacredito = new DocumentoContable(TipoDocumentoContable.NotaCredito, 12365);
+//Console.WriteLine(factura.Descripcion());
+//Console.WriteLine(notacredito.Descripcion());
+
+DocumentoContable factura = new Factura(12356);
 Console.WriteLine(factura.Descripcion());
-Console.WriteLine(notacredito.Descripcion());
+DocumentoContable notadebito = new NotaDebito(12356);
+Console.WriteLine(notadebito.Descripcion());
 Console.ReadKey();
 
